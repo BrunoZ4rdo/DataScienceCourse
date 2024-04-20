006d7708c7eb639575b1cfb750aa28ae3310194e
Bruno <bruno2xd@hotmail.com>
2024-04-20 19:59:42 -0300

    primeiro commit

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,36 @@
+#Data Science Tools and Ecosystem
+
+#In this notebook, Data Science Tools and Ecosystems are summarized.
+# This program is used to learn some simple arithmetics expressions. 
+## Author: Bruno Melim Zardo
+
+##  Data Science languages
+## 0-Python
+## 1-R
+## 2-SQL
+
+## Ordered list
+## 0-Pandas
+## 1-Matplotlib
+## 2-Numpy
+
+## Data Science Tools
+## 0-Pycharm
+## 1-Rstudio
+## 2-Jupyter IDE
+
+### Below are a few examples of evaluating arithmetic expressions in Python.
+### **Example 1: Basic Arithmetic**
+
+# Evaluate simple arithmetic expression
+result = 10 + 5
+print(result)
+
+### Example 2: simple multiply 
+result1= 4*5
+print(result1)
+
+### Example 3: minutes in hours
+min=200
+hour=min/60
+print(hour)
